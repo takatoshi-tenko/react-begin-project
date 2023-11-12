6c452bdbd657a1ba2d9c68a420a8fb20902c421a
takatoshi.tenko <chanten811@gmail.com>
2023-11-12 23:29:16 +0900

    add: リンクタグの挙動確認

--- my-react-first/src/App.js
+++ my-react-first/src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 
 // 関数コンポーネント
 function App() {
+
+  const attrs = {
+    href: 'https://wings.msn.to/',
+    download: false,
+    target: '_blank',
+    rel: 'help'
+  }
+
   return (
     <div className="App">
-      <a>
-        こんにちは！React
+      <a href={attrs.href} download={attrs.download} target={attrs.target} rel={attrs.rel}>
+        サポートページへ
+      </a>
+      <br/>
+      <br/>
+      <a {...attrs}>
+        サポートページへスプレッド構文
       </a>
     </div>
   );
