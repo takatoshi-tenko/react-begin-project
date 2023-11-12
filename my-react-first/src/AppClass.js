7e9c0ecbf6bb49a1b0a46b9d8cf9145a6d727a21
takatoshi.tenko <chanten811@gmail.com>
2023-11-12 23:09:32 +0900

    add: class構文のAppClass.jsを追加

---
+++ my-react-first/src/AppClass.js
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from "./logo.svg"
+import './App.css'
+
+// クラスコンポーネント
+class AppClass extends React.Component {
+  render() {
+    return (
+      <div className="App"></div>
+    )
+  }
+}
+export default AppClass
