fd62b31daafe24652fa5a4e525160e52c3602383
takatoshi.tenko <chanten811@gmail.com>
2023-11-12 20:05:20 +0900

    add: first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const value = 1_234_567
+console.log(value)
+const circle = redius => (redius * 2) * Math.PI
+console.log(circle(3))
+const title = '速習react'
+const price = 500
+const book = { title: title, price: price }
+console.log(book)
